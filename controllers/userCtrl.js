7ad3fcf4a06670462e64db59e0d1b9506c0b5402
ahmad_mac <amadghorbani@gmail.com>
2023-05-23 00:34:29 +0330

    set httpOnly to true

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -57,6 +57,7 @@ const userCtrl = {
       const refreshToken = createRefreshToken({ id: user._id });
 
       res.cookie("refreshToken", refreshToken, {
+        httpOnly:true,
         path: "/user/refresh_token",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       });
