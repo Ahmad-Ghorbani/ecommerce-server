71b383df7a88e19d5dd0db941e765025474671c1
ahmad_mac <amadghorbani@gmail.com>
2023-05-24 01:05:11 +0330

    revert headers origin

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  req.headers.origin('https://646d279ef8b23e06a23436a3--ninja-shopper.netlify.app/')
+  // req.headers.origin('https://646d279ef8b23e06a23436a3--ninja-shopper.netlify.app/')
   next();
 });
 
