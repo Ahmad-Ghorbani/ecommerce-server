e9dd4ee680134771c2fabc3be95fe76bfb65805c
ahmad_mac <amadghorbani@gmail.com>
2023-05-26 13:29:18 +0330

    add secure to cookie options

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -60,6 +60,7 @@ const userCtrl = {
         httpOnly:true,
         path: "/user/refresh_token",
         sameSite: 'none',
+        secure,
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       });
 
