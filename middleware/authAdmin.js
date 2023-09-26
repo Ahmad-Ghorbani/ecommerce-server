b0d97cdf7bf763c0ad7c3f662421c50a4fde6534
ahmad_mac <amadghorbani@gmail.com>
2023-09-27 02:02:39 +0330

    build: enable middlewares

--- middleware/authAdmin.js
+++ middleware/authAdmin.js
@@ -3,11 +3,11 @@ const Users = require("../models/userModel");
 const authAdmin = async (req, res, next) => {
   try {
     //find user information by id
-    // const user = await Users.findOne({
-    //   _id: req.user.id,
-    // });
-    // if (user.role === 0)
-    //   return res.status(400).json({ msg: "Admin resources access denied" });
+    const user = await Users.findOne({
+      _id: req.user.id,
+    });
+    if (user.role === 0)
+      return res.status(400).json({ msg: "Admin resources access denied" });
 
     next();
   } catch (err) {
